af3d284f83c709f42b393060b629225d33563163
Aniket Patel <aniketpatel1543@gmail.com>
2026-06-05 18:27:05 +0530

    chore: remove temporary regex-debug scripts

--- scripts/test-regex5.cjs
+++
@@ -1,6 +0,0 @@
-const s = String.raw`^(\s*)import\s*\{([^}]+)\}\s*from\s*["'](@/features/[a-z0-9-]+/server)["']\s*;?\s*$`;
-console.log("regex source:", s);
-const re = new RegExp(s, "gm");
-console.log("regex:", re);
-const test = 'import { x } from "@/features/test/server";';
-console.log("match:", test.match(re));
